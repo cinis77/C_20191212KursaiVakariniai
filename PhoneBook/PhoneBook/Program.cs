fecca60b5c1976cbecfa7cba561da5c8c872995e
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-12-19 21:25:53 +0200

    xml

---
+++ PhoneBook/PhoneBook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite telefona ir vardas atskirant kableliu");
+            var data = Console.ReadLine();
+            var splitData = data.Split(',');
+            long telefonas = 0;
+            Dictionary<string, long> telefonuKnyga = new Dictionary<string, long>();
+
+            if (long.TryParse(splitData.First(), out telefonas))
+            {
+                telefonuKnyga.Add(splitData.Last(), telefonas);
+            }
+            else
+            {
+                Console.WriteLine("Neteisingai iveste telefono reiksme");
+            }
+        }
+    }
+}
---
+++ XMLKlases/XMLKlases/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace XMLKlases
+{
+    public class ManoKlase
+    {
+        public int Pirmas { get; set; }
+        public string Antras { get; set; }
+
+        [XmlArray]
+        public List<DaugElementu> Elementai;
+    }
+
+    public class DaugElementu
+    {
+        public int AAA { get; set; }
+        public int BBB { get; set; }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(ManoKlase));
+            using (XmlReader reader = XmlReader.Create("machine.KCS"))
+            {
+                ManoKlase iten = (ManoKlase)xml.Deserialize(reader);
+                Console.WriteLine(iten.Antras + " " + iten.Pirmas);
+            }
+        }
+
+        private static void SukurtiXML()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(ManoKlase));
+
+            using (System.IO.FileStream stream = System.IO.File.Create(@"machine.KCS"))
+            {
+                ManoKlase objektas = new ManoKlase()
+                {
+                    Pirmas = 10,
+                    Antras = "Laba Diena"
+                };
+
+                xml.Serialize(stream, objektas);
+            }
+        }
+    }
+}
